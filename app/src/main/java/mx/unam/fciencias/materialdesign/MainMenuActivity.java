efa8fa978effee03b0c8604ef9434d39af5a499b
Camila Cruz <ccmiranda@ciencias.unam.mx>
2023-03-21 20:39:21 -0600

    Actividad del menú: 1:02:00-1:15:30.

--- app/src/main/java/mx/unam/fciencias/materialdesign/MainMenuActivity.java
+++ app/src/main/java/mx/unam/fciencias/materialdesign/MainMenuActivity.java
@@ -1,14 +1,72 @@
 package mx.unam.fciencias.materialdesign;
 
+import android.content.BroadcastReceiver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainMenuActivity extends AppCompatActivity {
+public abstract class MainMenuActivity extends AppCompatActivity {
+
+    public static final byte RESULT_EXIT = 2;
+
+    private final ActivityResultLauncher<Intent> resultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    int resultCode = result.getResultCode();
+                    Intent data = result.getData();
+                    if (resultCode == RESULT_EXIT) {
+                        finish();
+                    }
+                }
+            }
+    );
+
+    @Override
+    public Intent registerReceiver(@Nullable BroadcastReceiver receiver, IntentFilter filter) {
+        return super.registerReceiver(receiver, filter);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int intId = item.getItemId();
+        if (intId == R.id.menu_about) {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle(R.string.menu_about)
+                    .setIcon(android.R.drawable.ic_dialog_info)
+                    .setMessage(R.string.about)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .create().show();
+            return true;
+        }
+        //cerrar app:
+
+        return super.onOptionsItemSelected(item);
+    }
 }
