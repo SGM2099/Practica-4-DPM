8c6195f947a7e85cd5a71d37d8b28dce1817de12
Cervantes <marlboro@ciencias.unam.mx>
2023-04-10 17:12:36 -0600

    Implementando la vista de Detalle

--- app/src/main/java/mx/unam/fciencias/materialdesign/DetailActivity.java
+++ app/src/main/java/mx/unam/fciencias/materialdesign/DetailActivity.java
@@ -1,14 +1,34 @@
 package mx.unam.fciencias.materialdesign;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class DetailActivity extends AppCompatActivity {
+public class DetailActivity extends MainMenuActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        Intent starIntent = getIntent();
+        ActionBar actionBar = getSupportActionBar();
+        String detailFragmentTitle = null;
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(starIntent.getStringExtra(ENTRY_MESSAGE_KEY));
+        } else {
+            detailFragmentTitle = starIntent.getStringExtra(ENTRY_MESSAGE_KEY);
+        }
+        Bundle detailFragmentArgs = new Bundle();
+        detailFragmentArgs.putInt(DetailsFragment.INDEX_KEY,
+                startIntent.getIntExtra(DetailsFragment.INDEX_KEY, -1));
+        detailFragmentArgs.putInt(DetailsFragment.MASTER_LIST_SIZE_KEY,
+                startIntent.getIntExtra(DetailsFragment.MASTER_LIST_SIZE_KEY, -1));
+        DetailsFragment detailsFragment = new DetailsFragment();
+        detailsFragment.setArguments(detailFragmentArgs);
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.color_detail_holder, detailsFragment).commit();
     }
 }
--- app/src/main/java/mx/unam/fciencias/materialdesign/SecondActivity.java
+++ app/src/main/java/mx/unam/fciencias/materialdesign/SecondActivity.java
@@ -5,28 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class SecondActivity extends MainMenuActivity {
+public class SecondActivity extends MainMenuActivity implements InfiniteListAdapter.MasterListItemClickHandler {
 
-    InfiniteListAdapter listAdapter;
+    private InfiniteListAdapter listAdapter;
+    private boolean isDetailsPaneAvailable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         ActionBar actionBar = getSupportActionBar();
-
         if (actionBar != null) {
-
             actionBar.setDisplayHomeAsUpEnabled(true);
-
         }
-
-
-
         setContentView(R.layout.activity_second);
         Button button = findViewById(R.id.add_to_list_button);
         button.setOnClickListener(this::addListElement);
@@ -34,14 +30,39 @@ public class SecondActivity extends MainMenuActivity {
         infiniteList.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         infiniteList.setLayoutManager(layoutManager);
-        listAdapter = new InfiniteListAdapter(getResources());
+        listAdapter = new InfiniteListAdapter(getResources(), this);
         addListElement(null);
         infiniteList.setAdapter(listAdapter);
+        isDetailsPaneAvailable = findViewById(R.id.color_detail_holder) != null;
 
     }
-
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && isDetailsPaneAvailable) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
+        }
+    }
     public void addListElement(View button){
         listAdapter.addItem();
     }
+    @Override
+    public void onItemClicked(int clickItemIndex, String entryText, int masterListSize) {
+        if (isDetailsPaneAvailable) {
+            Bundle detailsFragmentArgs = new Bundle();
+            detailsFragmentArgs.putInt(DetailsFragment.INDEX_KEY, clickItemIndex);
+            detailsFragmentArgs.putInt(DetailsFragment.MASTER_LIST_SIZE_KEY, clickItemIndex);
+            DetailsFragment detailsFragment = new DetailsFragment();
+            detailsFragment.setArguments(detailsFragmentArgs);
+            getSupportFragmentManager().beginTransaction().replace(
+                    R.id.color_detail_holder, detailsFragment).commit();
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailsFragment.INDEX_KEY, clickItemIndex);
+            intent.putExtra(DetailsFragment.ENTRY_MESSAGE_KEY, clickItemIndex);
+            intent.putExtra(DetailsFragment.MASTER_LIST_SIZE_KEY, clickItemIndex);
+            startActivity(intent);
 
+        }
+    }
 }
