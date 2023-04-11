235b37e788b47da6203b78059deae832d6558871
Gonzalo209 <cgonzalo983@gmail.com>
2023-04-10 23:46:29 -0600

    ultima parte

--- app/src/main/java/mx/unam/fciencias/materialdesign/SettingsActivity.java
+++ app/src/main/java/mx/unam/fciencias/materialdesign/SettingsActivity.java
@@ -1,16 +1,62 @@
 package mx.unam.fciencias.materialdesign;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends MainMenuActivity
+    implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         getSupportFragmentManager().beginTransaction().add(
                 android.R.id.content, new SettingsFragment()
         ).commit();
+
     }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+
+        super.onPause();
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+
+        if(! themePreferenceKey.equals(s)) return;
+
+        String selectedPreference = sharedPreferences.getString(s, lightThemeId);
+
+        applyTheme(selectedPreference, true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId() == R.id.menu_settings) return true;
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
 }
